d7dc73dce3c3fe65545796900651a40888feadea
yuzhq <1065265077@qq.com>
2018-12-08 10:25:42 +0800

    Default Changelist

---
+++ src/Student.java
@@ -0,0 +1,26 @@
+public class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Array<Student> studentArray=new Array<Student>();
+        studentArray.addLast(new Student("ben",17));
+        studentArray.addLast(new Student("pen",17));
+        studentArray.addLast(new Student("ren",17));
+        System.out.println(studentArray);
+
+    }
+}
