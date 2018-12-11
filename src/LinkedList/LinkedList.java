24d4784bd93b74d7fe1e2f241f71a793e459c2f4
yuzq <yuzq@ngarihealth.com>
2018-12-11 09:57:23 +0800

    linkedList

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,39 @@
+package LinkedList;
+
+public class LinkedList<E>{
+    private class Node{
+        public E e;
+        public Node next;
+
+        public Node(E e,Node next){
+            this.e=e;
+            this.next=next;
+        }
+
+        public Node(E e){
+            this(e,null);
+        }
+
+        public Node(){
+            this(null,null);
+        }
+
+        @Override
+        public String toString(){
+            return e.toString();
+        }
+    }
+    private Node head;
+    private int size;
+
+    public LinkedList(){
+        head=null;
+        size=0;
+    }
+    public int getSize(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size==0;
+    }
+}
